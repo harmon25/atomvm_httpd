8c1907aeaf9e8ecbd1fee1b575a7bb97c31cf086
harmon25 <dougwright1@gmail.com>
2026-01-11 17:59:07 -0500

    Implement gzip support in handle_http_req; add get_accept_encoding and accepts_gzip functions for improved file serving

--- src/httpd_file_handler.erl
+++ src/httpd_file_handler.erl
@@ -36,7 +36,7 @@ init_handler(PathSuffix, HandlerConfig) ->
     {ok, #state{path_suffix = PathSuffix, handler_config = HandlerConfig}}.
 
 %% @hidden
-handle_http_req(#{method := get} = _HttpRequest, State) ->
+handle_http_req(#{method := get} = HttpRequest, State) ->
     HandlerConfig = State#state.handler_config,
     App = maps:get(app, HandlerConfig),
     PathSuffix = State#state.path_suffix,
@@ -47,14 +47,11 @@ handle_http_req(#{method := get} = _HttpRequest, State) ->
     end,
     FullPath = join("/", lists:reverse(ResolvedPath)),
     ?TRACE("App: ~p PathSuffix: ~p FullPath: ~p", [App, ResolvedPath, FullPath]),
+    %% Check if client accepts gzip encoding
+    AcceptEncoding = get_accept_encoding(HttpRequest),
+    AcceptsGzip = accepts_gzip(AcceptEncoding),
     try
-        case atomvm:read_priv(App, FullPath) of
-            undefined ->
-                io:format("httpd_file_handler: file not found - app=~p path=~p~n", [App, FullPath]),
-                {error, not_found};
-            Data when is_binary(Data) ->
-                {close, #{"Content-Type" => get_content_type(lists:reverse(ResolvedPath))}, Data}
-        end
+        serve_file(App, FullPath, ResolvedPath, AcceptsGzip)
     catch
         _:Reason ->
             io:format("httpd_file_handler: error reading file ~p: ~p~n", [FullPath, Reason]),
@@ -62,6 +59,49 @@ handle_http_req(#{method := get} = _HttpRequest, State) ->
     end;
 handle_http_req(_HttpRequest, _HandlerConfig) ->
     {error, internal_server_error}.
+
+%% @private
+%% Try to serve gzipped version if available and client accepts it
+serve_file(App, FullPath, ResolvedPath, true = _AcceptsGzip) ->
+    GzPath = FullPath ++ ".gz",
+    case atomvm:read_priv(App, GzPath) of
+        undefined ->
+            %% No gzipped version, serve original
+            serve_file(App, FullPath, ResolvedPath, false);
+        Data when is_binary(Data) ->
+            %% Serve gzipped version with Content-Encoding header
+            ContentType = get_content_type(lists:reverse(ResolvedPath)),
+            {close, #{"Content-Type" => ContentType, "Content-Encoding" => "gzip"}, Data}
+    end;
+serve_file(App, FullPath, ResolvedPath, false = _AcceptsGzip) ->
+    case atomvm:read_priv(App, FullPath) of
+        undefined ->
+            io:format("httpd_file_handler: file not found - app=~p path=~p~n", [App, FullPath]),
+            {error, not_found};
+        Data when is_binary(Data) ->
+            {close, #{"Content-Type" => get_content_type(lists:reverse(ResolvedPath))}, Data}
+    end.
+
+%% @private
+get_accept_encoding(#{headers := Headers}) ->
+    %% Try various header name formats
+    case maps:get(<<"Accept-Encoding">>, Headers, undefined) of
+        undefined ->
+            case maps:get(<<"accept-encoding">>, Headers, undefined) of
+                undefined -> <<>>;
+                Val -> Val
+            end;
+        Val -> Val
+    end;
+get_accept_encoding(_) ->
+    <<>>.
+
+%% @private
+accepts_gzip(AcceptEncoding) when is_binary(AcceptEncoding) ->
+    %% Simple check - look for "gzip" in the Accept-Encoding header
+    binary:match(AcceptEncoding, <<"gzip">>) =/= nomatch;
+accepts_gzip(_) ->
+    false.
 
 %% @private
 join(Separator, Path) ->

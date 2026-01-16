4436e77e8839176d199c320a475e124e2010fcb7
harmon25 <dougwright1@gmail.com>
2026-01-16 16:34:05 -0500

    rework web assets - removes jquery,backbone + css file

--- examples/elixir_http/priv/js/app.js
+++ examples/elixir_http/priv/js/app.js
@@ -10,111 +10,132 @@
 //     # ----------------------------------------------------------------------------
 //
 
-var SystemModel = Backbone.Model.extend({
-	url: '/api/system_info',
-	defaults: {
-		platform: null,
-		system_architecture: null,
-		word_size: null,
-		atomvm_version: null,
-		esp32_chip_info: null,
-		esp_idf_version: null,
-		chip_model: null,
-		chip_cores: null,
-		chip_features: null,
-		chip_revision: null
-	},
+// Vanilla JS implementation for AtomVM Web Console
 
-	parse: function(data) {
-		data["chip_model"] = data.esp32_chip_info["model"];
-		data["chip_cores"] = data.esp32_chip_info["cores"];
-		data["chip_features"] = data.esp32_chip_info["features"];
-		data["chip_revision"] = data.esp32_chip_info["revision"];
-		return data;
-	}
-});
-var system_model = new SystemModel();
-
-var SystemView = Backbone.View.extend({
-	el: '#system-view',
-	template: _.template($('#system-tmpl').html()),
-
-	initialize: function() {
-		this.listenTo(this.model, 'sync change', this.render);
-		this.model.fetch();
-		this.render();
-	},
-
-	render: function() {
-		var html = this.template(this.model.toJSON());
-		this.$el.html(html);
-		return this;
-	}
-});
-var system_view = new SystemView({model: system_model})
-
-var MemoryModel = Backbone.Model.extend({
-	url: '/api/memory',
-	defaults: {
-		atom_count: null,
-		port_count: null,
-		process_count: null,
-		esp32_free_heap_size: null,
-		esp32_largest_free_block: null,
-		esp32_minimum_free_size: null
-	},
-
-	parse: function(data) {
-		return data;
-	}
-});
-var memory_model = new MemoryModel();
-
-var MemoryView = Backbone.View.extend({
-	el: '#memory-view',
-	template: _.template($('#memory-tmpl').html()),
-
-	initialize: function() {
-		this.listenTo(this.model, 'sync change', this.render);
-		this.model.fetch();
-		this.render();
-	},
-
-	render: function() {
-		var html = this.template(this.model.toJSON());
-		this.$el.html(html);
-		return this;
-	}
-});
-var memory_view = new MemoryView({model: memory_model})
-
-var get_websocket_url = function() {
-	var hostname = window.location.hostname;
-	var port = window.location.port;
-	return "ws://" + hostname + ":" + port + "/ws";
+// --- System Info Fetch and Render ---
+function fetchSystemInfo() {
+	fetch('/api/system_info')
+		.then(function(response) { return response.json(); })
+		.then(function(data) {
+			// If esp32_chip_info exists, flatten it
+			if (data.esp32_chip_info) {
+				data.chip_model = data.esp32_chip_info.model;
+				data.chip_cores = data.esp32_chip_info.cores;
+				data.chip_features = data.esp32_chip_info.features;
+				data.chip_revision = data.esp32_chip_info.revision;
+			}
+			renderSystemInfo(data);
+		})
+		.catch(function(err) {
+			console.log(err)
+			document.getElementById('system-view').innerHTML = '<div style="color:red">Failed to load system info</div>';
+		});
 }
 
-var create_websocket = function() {
-	var ws = new WebSocket(get_websocket_url(), []);
-	ws.onmessage = function (event) {
-		console.log("Received WS data: " + event.data);
-		var data = JSON.parse(event.data);
-		for (var key in memory_model.attributes) {
-			if (data[key]) {
-				memory_model.set(key, data[key]);
+function renderSystemInfo(data) {
+	var html = `<table>
+		<tr><td>platform</td><td>${data.platform ?? ''}</td></tr>
+		<tr><td>system_architecture</td><td>${data.system_architecture ?? ''}</td></tr>
+		<tr><td>word_size</td><td>${data.word_size ?? ''}</td></tr>
+		<tr><td>chip_model</td><td>${data.chip_model ?? ''}</td></tr>
+		<tr><td>chip_cores</td><td>${data.chip_cores ?? ''}</td></tr>
+		<tr><td>chip_features</td><td>${data.chip_features ?? ''}</td></tr>
+		<tr><td>chip_revision</td><td>${data.chip_revision ?? ''}</td></tr>
+		<tr><td>atomvm_version</td><td>${data.atomvm_version ?? ''}</td></tr>
+		<tr><td>esp_idf_version</td><td>${data.esp_idf_version ?? ''}</td></tr>
+	</table>`;
+	document.getElementById('system-view').innerHTML = html;
+}
+
+// --- Memory Info Fetch and Render ---
+let memoryData = {};
+function fetchMemoryInfo() {
+	fetch('/api/memory')
+		.then(function(response) { return response.json(); })
+		.then(function(data) {
+			memoryData = data;
+			renderMemoryInfo(data);
+		})
+		.catch(function(err) {
+			document.getElementById('memory-view').innerHTML = '<div style="color:red">Failed to load memory info</div>';
+		});
+}
+
+function renderMemoryInfo(data) {
+	var html = `<table>
+		<tr><td>atom_count</td><td>${data.atom_count ?? ''}</td></tr>
+		<tr><td>port_count</td><td>${data.port_count ?? ''}</td></tr>
+		<tr><td>process_count</td><td>${data.process_count ?? ''}</td></tr>
+		<tr><td>esp32_free_heap_size</td><td>${data.esp32_free_heap_size ?? ''}</td></tr>
+		<tr><td>esp32_largest_free_block</td><td>${data.esp32_largest_free_block ?? ''}</td></tr>
+		<tr><td>esp32_minimum_free_size</td><td>${data.esp32_minimum_free_size ?? ''}</td></tr>
+	</table>`;
+	document.getElementById('memory-view').innerHTML = html;
+}
+
+// --- WebSocket for live memory updates ---
+function getWebSocketUrl() {
+	var hostname = window.location.hostname;
+	var port = window.location.port;
+	return "ws://" + hostname + (port ? ":" + port : "") + "/ws";
+}
+
+function createWebSocket() {
+	var ws = new window.WebSocket(getWebSocketUrl());
+	ws.onmessage = function(event) {
+		try {
+			var data = JSON.parse(event.data);
+			// Only update known memory fields
+			let changed = false;
+			for (var key in data) {
+				if (Object.prototype.hasOwnProperty.call(memoryData, key) && data[key] !== memoryData[key]) {
+					memoryData[key] = data[key];
+					changed = true;
+				}
 			}
+			if (changed) {
+				renderMemoryInfo(memoryData);
+			}
+		} catch (e) {
+			// ignore parse errors
 		}
-	}
-
-	ws.onopen = function (event) {
-		console.log("opened connection: " + event);
-	}
-
-	ws.onclose = function (event) {
-		console.log("closed connection: " + event);
-		webSocket = create_websocket();
-	}
+	};
+	ws.onopen = function(event) {
+		console.log("WebSocket opened");
+	};
+	ws.onclose = function(event) {
+		console.log("WebSocket closed, reconnecting...");
+		setTimeout(createWebSocket, 2000);
+	};
+	ws.onerror = function(event) {
+		console.log("WebSocket error");
+	};
 	return ws;
 }
 
-var webSocket = create_websocket();
+// --- Initialization ---
+document.addEventListener('DOMContentLoaded', function() {
+	// Hide loader and show content after 1s
+	setTimeout(function() {
+		var loader = document.getElementById('loader');
+		var content = document.getElementById('content');
+		if (loader) loader.style.display = 'none';
+		if (content) content.style.display = 'block';
+	}, 1000);
+
+	// Tab switching logic
+	var tabBtns = document.querySelectorAll('.tab-btn');
+	tabBtns.forEach(function(btn) {
+		btn.addEventListener('click', function() {
+			tabBtns.forEach(function(b) { b.classList.remove('active'); });
+			document.querySelectorAll('.panel').forEach(function(p) { p.classList.remove('active'); });
+			btn.classList.add('active');
+			var panel = document.getElementById(btn.getAttribute('data-panel'));
+			if (panel) panel.classList.add('active');
+		});
+	});
+
+	fetchSystemInfo();
+	fetchMemoryInfo();
+	createWebSocket();
+});
